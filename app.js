145ba0122fd1a8d6d1fc2628a3f47cefbbadf6bd
Chris <chewning.cl@gmail.com>
2018-06-26 22:53:02 -0500

    fixed a lot of CSS, got most things connected, and turned the lights off.

--- app.js
+++ app.js
@@ -12,27 +12,125 @@ let age = 30;
 let hunger = 5;
 let boredom = 5;
 let sleepiness = 5;
+// let lightsOn = true;
+
+
+//-------------------LIGHTS FUNCTIONS----------------------------
+
+
+//adapted from: http://jsfiddle.net/S4rsh/
+const lightsOff = () => {
+  $('#tamaImg').css({
+    'background-color': 'black'
+  })
+  // }).fadeTo(1000, 0.0);
+  //fadeTo() is the method. 1000 is milliseconds? 0.0 is transparency.
+};
+
+const lightsReallyOff = () => {
+  const $body = $('body');
+  $('body').css({
+    'background-color': 'black'
+  })
+};
+
+const lightsOn = () => {
+  $('#tamagImg').show().css()
+    // 'display': 'flex',
+    // 'flex-direction': 'column',
+    // 'justify-content': 'space-between',
+    // 'align-items': 'center'
+  // })
+};
+
+
+//could just toggle lightsOn to false.
+
+// let lightsOn = () {
+//
+// };
+
+// let lightsOff = () => {
+//   //could jut be toggling lightOn back on.
+// }
+
+//When you click something the lights turn back on.
+// $('#').on('click', (e) => {
+//   const $hungerLevel = $('#hungerLevel');
+//   console.log('I am hungry'); //working
+// });
+// });
+
+
+
+
 
 
 
 //TIMER FUNCTION
+
+
+
+//
+// $('#submit-btn').on('click', (e) => {
+//   //don't forget the e ni the parameter. if it's blank it won't read the preventDefault()
+//   e.preventDefault();
+// console.log($('#input-box').val()); //checking
+//
+// const $tama = $('#tama').val(); //this gets the value you put in the input box with id tama
+// // $('#tama').text(`${tama}`).val('');
+// $('#tama').append($('#input-box').val() + "'s ");
+// //gets the input value. you want that to be in a string with 's though.
+// //cannot do it opposite. ex: $('#input-box').val().appendTo('#tama');
+//
+//
+// //DOESN'T WORK TO EMPTY THE INPUT FORM
+// // render() {
+// //     $('#input-box').empty();
+// //     e.preventDefault();
+// // }
+
+
+
+
 const setTimer = () => {
   const $timer = $('#timer');
   let $age = $('#age');
+  let $hunger = $('#hunger');
+  let $boredom = $('#boredom');
+  let $sleepiness = $('#sleepiness');
+
   const timer = setInterval(() => {
 time--;
-console.log(time);
+// console.log(time); //checking
 
   if (time === 50) {
+    lightsOff();
   age += 10; //only works on this one.
-  console.log(age);
+  console.log(age + 'age');
+  hunger += 4;
+  console.log(hunger + 'hunger');
+  boredom +=5;
+  console.log(boredom + 'boredom');
+  sleepiness += 1;
+  console.log(sleepiness + 'sleepiness');
+
+} else if (time === 47){
+  // lightsOn();
+  console.log('hi');
+  lightsReallyOff();
 } else if (time === 40){
   age += 10;
-
+  lightsOff();
   console.log(age);
 } else if (time === 30) {
   age += 10;
   console.log(age);
+  lightsOff();
+  sleepiness === 10;
+  boredom === 10;
+  //when lights go off your sleepiness is 10 and your boredom is 10. you have to bring these down by 25 seconds or the game is over.
+
 } else if (time === 20) {
   age += 10;
   console.log(age);
@@ -45,13 +143,15 @@ console.log(time);
 };
 
 $('#timer').text('timer: ' + time + 's');
-$('#age').text('age: ' + age + ' years old.')
+$('#age').text($('#input-box').val() + ' is ' + age + ' years old.')
 
 }, 1000); //this goes every second.
 };
 
+// console.log($('#input-box').val());
 
-$('#start').on('click', (e) => {
+
+$('#submit-btn').on('click', (e) => {
   console.log('button clicked'); //testing
 setTimer();
 });
@@ -87,7 +187,7 @@ console.log(tama);
 
 
 
-// -------------------------------BUTTONS AND THE FUNCTIONS THAT GO WITH THEM-------------------------------
+// -----------------BUTTONS AND THE FUNCTIONS THAT GO WITH THEM-----------------
 
 //OTHER BUTTONS
 // const $boredomButton = $('<button id="boredomLevel">Decrease Boredom</button>');
@@ -163,39 +263,22 @@ console.log($('#input-box').val());
 
 const $tama = $('#tama').val(); //this gets the value you put in the input box with id tama
 // $('#tama').text(`${tama}`).val('');
-$('#tama').append($('#input-box').val() + "'s ");
+// $('#tama').append($('#input-box').val() + " is ");
 //gets the input value. you want that to be in a string with 's though.
 //cannot do it opposite. ex: $('#input-box').val().appendTo('#tama');
 
 
+//DOESN'T WORK TO EMPTY THE INPUT FORM
 // render() {
 //     $('#input-box').empty();
 //     e.preventDefault();
 // }
 
 
-// $('#tama').append(`${tama}`);
-
-// const favTvShow = $('#input-box2').val();
-//   $('body').append(`Your favourite TV show is ${favTvShow}`);
-
-
 });
 
 
 // const $hungerLevel = $('#hungerLevel');
-
-
-
-
-
-
-
-// $('form').on('submit', (e) => {
-//   e.preventDefault(); //e.preventDefault() stops the form from trying to refresh the page.
-//   console.log($('#input-box3').val());
-//
-// });
 
 
 // const $sleepyButton = $('<button id="sleepinessLevel">Decrease Sleepiness</button>');
@@ -243,10 +326,13 @@ $('#tama').append($('#input-box').val() + "'s ");
 //
 //
 //
-//
+
+/*accessing the Class. could you just say
+(hunger === 10 || sleepiness === 10 || boredom === 10)*/
 // const didPetDie = (timerName) => {
 // 		if (pet.hunger === 10 || pet.sleepiness === 10 || pet.boredom === 10) {
-// 			console.log(`{($('#input-box').val())}`);
+// 			console.log(`{($('#input-box').val())}`); //to check
+//        alert($('#input-box').val() + ' has died.')
 // 			clearInterval(time)
 // 		}
 // 	};
